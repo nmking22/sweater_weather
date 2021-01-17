ab283135a7db24fcbaf8076275672faa534ca1ff
Nick King <nickmaxking@gmail.com>
2021-01-17 16:35:21 -0700

    Add sad path conditionals to index action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,10 +7,16 @@ class Api::V1::UsersController < ApplicationController
       @api_key = SecureRandom.base64
       break if User.where(api_key: @api_key) == []
     end
-    user = User.create(user_params.merge({api_key: @api_key}))
+    user = User.new(user_params.merge({api_key: @api_key}))
 
-    output = UsersSerializer.new(user)
-    render json: output
+    if user.save
+      output = UsersSerializer.new(user)
+      render json: output, :status => 201
+    else
+      output = Hash.new
+      output[:error] = user.errors.full_messages[0]
+      render json: output, :status => 400
+    end
   end
 
   private
