2dc82efe0fc410d484fd85044f3948f3c605c1e8
Nick King <nickmaxking@gmail.com>
2021-01-17 17:37:23 -0700

    Add route and controller action for sessions post endpoint

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user != nil && user.authenticate(params[:password])
+      output = UsersSerializer.new(user)
+      render json: output
+    else
+      # return 400
+    end
+  end
+
+  private
+
+    def sessions_params
+      params.permit(:email, :password)
+    end
+end
