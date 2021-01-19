d9f61bc1dc19d93fbc5f8b448020f4dc316d258e
Nick King <nickmaxking@gmail.com>
2021-01-19 10:21:33 -0700

    Add find_trip_and_time class method

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -10,4 +10,23 @@ class RoadTripFacade
       RoadTrip.new('impossible', params[:origin], params[:destination], nil)
     end
   end
+
+  def self.find_trip_and_time(params)
+    trip_data = LocationService.find_trip_data(params)
+    time = trip_data[:route][:realTime]
+    if time
+      coordinates = LocationFacade.find_coordinates(params[:destination])
+      weather_at_eta = WeatherFacade.find_weather_at_eta(coordinates, time)
+      trip = RoadTrip.new(time, params[:origin], params[:destination], weather_at_eta)
+      trip_and_time = {
+        trip: trip,
+        time: time
+      }
+    else
+      trip_and_time = {
+        trip: RoadTrip.new('impossible', params[:origin], params[:destination], nil),
+        time: 'impossible'
+      }
+    end
+  end
 end
