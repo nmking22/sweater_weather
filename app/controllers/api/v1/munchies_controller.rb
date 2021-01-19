73ca8170021d42f9f120a6a615fcbee6427aa133
Nick King <nickmaxking@gmail.com>
2021-01-19 11:46:06 -0700

    Refactor index action with helper methods

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,18 +1,25 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
+    munchie = Munchie.new(trip_and_time[:trip], restaurant)
+    output = MunchieSerializer.new(munchie)
+    
+    render json: output
+  end
+
+  def trip_and_time
     road_trip_params = {
       origin: munchies_params[:start],
       destination: munchies_params[:end]
     }
-    trip_and_time = RoadTripFacade.find_trip_and_time(road_trip_params)
+    RoadTripFacade.find_trip_and_time(road_trip_params)
+  end
+
+  def restaurant
     restaurant_query = {
       trip_and_time: trip_and_time,
       food: munchies_params[:food]
     }
-    restaurant = RestaurantFacade.find_restaurant(restaurant_query)
-    munchie = Munchie.new(trip_and_time[:trip], restaurant)
-    output = MunchieSerializer.new(munchie)
-    render json: output
+    RestaurantFacade.find_restaurant(restaurant_query)
   end
 
   private
