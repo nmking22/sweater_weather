87665052adc4141e3b2df23e7214e9bd4d9b9c36
Nick King <nickmaxking@gmail.com>
2021-01-19 10:57:34 -0700

    Modify hash keys with new syntax

--- app/services/restaurant_service.rb
+++ app/services/restaurant_service.rb
@@ -2,13 +2,13 @@ require 'date'
 
 class RestaurantService
   def self.find_restaurant_data(restaurant_query)
-    travel_time = restaurant_query[:road_trip][:time]
+    travel_time = restaurant_query[:trip_and_time][:time]
     eta = RestaurantService.convert_time(travel_time)
     response = conn.get('/v3/businesses/search') do |req|
       req.headers['Authorization'] = ENV['YELP_KEY']
       req.params['term'] = restaurant_query[:food]
       req.params['open_at'] = eta
-      req.params['location'] = restaurant_query[:road_trip][:trip].end_city
+      req.params['location'] = restaurant_query[:trip_and_time][:trip].end_city
     end
     JSON.parse(response.body, symbolize_names: true)[:businesses][0]
   end
