a510116868825f3211e8c7316a8bddf66ba04d14
Nick King <nickmaxking@gmail.com>
2021-01-19 10:41:57 -0700

    Add restaurant creation

--- app/facades/restaurant_facade.rb
+++ app/facades/restaurant_facade.rb
@@ -1,6 +1,6 @@
 class RestaurantFacade
   def self.find_restaurant(restaurant_query)
-    RestaurantService.find_restaurant_data(restaurant_query)
-    # binding.pry
+    restaurant_data = RestaurantService.find_restaurant_data(restaurant_query)
+    Restaurant.new(restaurant_data)
   end
 end
