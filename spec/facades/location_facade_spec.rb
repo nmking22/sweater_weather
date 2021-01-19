07f9b3fa0586ca5794f2b560a95b31515ffefc94
Nick King <nickmaxking@gmail.com>
2021-01-18 20:57:09 -0700

    Add tests for international locations and full addresses

--- spec/facades/location_facade_spec.rb
+++ spec/facades/location_facade_spec.rb
@@ -12,5 +12,43 @@ describe 'location facade' do
       expect(coordinates).to have_key(:lng)
       expect(coordinates[:lng]).to be_a(Float)
     end
+
+    it 'find_coordinates international locations' do
+      location_1 = 'london,uk'
+      location_2 = 'frankfurt,de'
+      coordinates_1 = LocationFacade.find_coordinates(location_1)
+      coordinates_2 = LocationFacade.find_coordinates(location_2)
+
+      expect(coordinates_1).to be_a(Hash)
+      expect(coordinates_1).to have_key(:lat)
+      expect(coordinates_1[:lat]).to be_a(Float)
+      expect(coordinates_1).to have_key(:lng)
+      expect(coordinates_1[:lng]).to be_a(Float)
+
+      expect(coordinates_2).to be_a(Hash)
+      expect(coordinates_2).to have_key(:lat)
+      expect(coordinates_2[:lat]).to be_a(Float)
+      expect(coordinates_2).to have_key(:lng)
+      expect(coordinates_2[:lng]).to be_a(Float)
+    end
+
+    it 'find_coordinates full address' do
+      location_1 = '1331 17th St LL100, Denver, CO 80202'
+      location_2 = '1600 Pennsylvania Avenue NW, Washington, DC 20500'
+      coordinates_1 = LocationFacade.find_coordinates(location_1)
+      coordinates_2 = LocationFacade.find_coordinates(location_2)
+
+      expect(coordinates_1).to be_a(Hash)
+      expect(coordinates_1).to have_key(:lat)
+      expect(coordinates_1[:lat]).to be_a(Float)
+      expect(coordinates_1).to have_key(:lng)
+      expect(coordinates_1[:lng]).to be_a(Float)
+
+      expect(coordinates_2).to be_a(Hash)
+      expect(coordinates_2).to have_key(:lat)
+      expect(coordinates_2[:lat]).to be_a(Float)
+      expect(coordinates_2).to have_key(:lng)
+      expect(coordinates_2[:lng]).to be_a(Float)
+    end
   end
 end
