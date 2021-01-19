09ee92a852548561add5d625f250059344972df0
Nick King <nickmaxking@gmail.com>
2021-01-18 20:49:19 -0700

    Add sad path for missing parameter

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,9 +1,17 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    location = params[:location]
-    forecast = ForecastFacade.find_forecast(location)
-    output = ForecastSerializer.new(forecast)
+    if params[:location]
+      location = params[:location]
+      forecast = ForecastFacade.find_forecast(location)
+      output = ForecastSerializer.new(forecast)
 
-    render json: output
+      render json: output
+    else
+      output = Hash.new
+      output[:error] = 'Location parameter is required.'
+
+      render json: output, :status => 400
+    end
+
   end
 end
