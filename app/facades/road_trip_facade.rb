066f89b155680587f260c72a1825a13cb3c2198c
Nick King <nickmaxking@gmail.com>
2021-01-19 17:27:11 -0700

    Refactor trip creation into helper method

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -3,11 +3,15 @@ class RoadTripFacade
     trip_data = LocationService.find_trip_data(params)
     time = trip_data[:route][:realTime]
     if time
-      coordinates = LocationFacade.find_coordinates(params[:destination])
-      weather_at_eta = WeatherFacade.find_weather_at_eta(coordinates, time)
-      RoadTrip.new(time, params[:origin], params[:destination], weather_at_eta)
+      self.possible_trip(params, time)
     else
       RoadTrip.new('impossible', params[:origin], params[:destination], nil)
     end
+  end
+
+  def self.possible_trip(params, time)
+    coordinates = LocationFacade.find_coordinates(params[:destination])
+    weather_at_eta = WeatherFacade.find_weather_at_eta(coordinates, time)
+    RoadTrip.new(time, params[:origin], params[:destination], weather_at_eta)
   end
 end
